3876574bd030627e1932e392f3945006189e52ff
Adamo1209 <fucai1209@qq.com>
2026-08-17 23:47:26 +0800

    将token配置迁移到环境变量 (.env) --- ### refactor(config): 将token配置迁移到环境变量 (.env) - 移除src/token.js文件，改用环境变量VITE_TIANDITU_KEY来存储天地图key - 更新.gitignore文件以忽略.env文件和vercel相关文件 - 删除token.js.holder文件中的注释内容

--- src/utils/init.js
+++ src/utils/init.js
@@ -1,14 +1,12 @@
 // vue
 import { ref } from "vue"
-// token
-import { token_tianditu_key } from "../token.js"
 
 // export
 // 初始 tab
 export const default_active = "BaseMapTab"
 export const fragment_tab = ref(default_active)
 // 天地图 key
-export const tianditu_key = token_tianditu_key
+export const tianditu_key = import.meta.env.VITE_TIANDITU_KEY || ""
 // 初始化天地图底图
 export const tianditu_basemap_init = "tianditu_vector"
 // 天地图底图对应映射:
